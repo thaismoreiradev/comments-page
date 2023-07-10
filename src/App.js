d39e56dcaea8c5691cccc51618592a8c3bc0c51a
thaismoreiradev <contatothaismoreira@hotmail.com>
2023-07-10 20:36:52 -0300

    basic js logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import './App.css';
-import { TbMessageHeart } from 'react-icons/tb';
+import React from "react";
+import "./App.css";
+import { TbMessageHeart } from "react-icons/tb";
 
 function App() {
+  const [currentComment, setCurrentComment] = React.useState("");
+  const [allComments, setAllComments] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  // const getValue = (data) => {
+  //   setCurrentComment(data.target.value)
+
+  // }
+
+  const handleButton = () => {
+    if (currentComment.length === 0) {
+      setErrorMessage("You need to write something first");
+    } else {
+      const commentsArray = [...allComments, currentComment];
+
+      setAllComments(commentsArray);
+
+      setCurrentComment("");
+
+      setErrorMessage();
+    }
+  };
+
   return (
-    <div className='container'>
-      <TbMessageHeart/>
+    <div className="container">
+      <TbMessageHeart />
       <h1>Send your comment</h1>
-      <input></input>
-      <button>SEND</button>
+      <textarea
+        onChange={(e) => setCurrentComment(e.target.value)}
+        value={currentComment}
+        placeholder="Enter your opinion here ..."
+      ></textarea>
+      <button onClick={handleButton}>SEND</button>
+      <p>{errorMessage}</p>
       <ul>
-        <li>comment 1</li>
-        <li>comment 2</li>
+        {allComments.map((comment, index) => (
+          <li key={index}>{comment}</li>
+        ))}
       </ul>
-    
     </div>
   );
 }
